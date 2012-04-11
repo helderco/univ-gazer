f93c26e66ee8fe3dd3b9034a7d6e257c29f01c85
Helder Correia <me@heldercorreia.com>
2012-04-11 02:27:49 +0000

    Restrict category selection to enabled ones.

--- src/Siriux/GalleryBundle/Form/Type/ImageType.php
+++ src/Siriux/GalleryBundle/Form/Type/ImageType.php
@@ -4,15 +4,22 @@ namespace Siriux\GalleryBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
+use Doctrine\ORM\EntityRepository;
 
 class ImageType extends AbstractType
 {
     public function buildForm(FormBuilder $builder, array $options)
     {
+        $enabledGalleries = function(EntityRepository $er) {
+                               return $er->createQueryBuilder('g')
+                                    ->where('g.enabled = 1');
+        };
+
         $builder
             ->add('gallery', 'entity', array(
                 'class' => 'SiriuxGalleryBundle:Gallery',
-                'empty_value' => '-- Choose a category --'))
+                'empty_value' => '-- Choose a category --',
+                'query_builder' => $enabledGalleries))
             ->add('media', 'siriux_media_type')
         ;
     }
